76a26fa063f2c03d11be5e2e860b3cbf931132a3
hhiroki <gyouzakuuka@gmail.com>
2019-12-22 21:06:51 +0900

    add btn click listner

--- app/src/main/java/com/example/hhiroki/multilingualhello/MainActivity.kt
+++ app/src/main/java/com/example/hhiroki/multilingualhello/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.hhiroki.multilingualhello
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // event listener
+        morningBtn.setOnClickListener {
+            greetingText.text = getString(R.string.good_morning_text)
+        }
+        afternoonBtn.setOnClickListener {
+            greetingText.text = getString(R.string.good_afternoon_text)
+        }
+        eveningBtn.setOnClickListener {
+            greetingText.text = getString(R.string.good_evening_text)
+        }
     }
 }
